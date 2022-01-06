a9ec76f507f246625fd3a19960555d2fa1b71e3b
Cristian Henrici <cristian.henrici@itdistrict.ch>
2022-01-06 14:16:22 +0100

    added TestVue

---
+++ packages/remote/src/components/TestVue.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>This message in microfrontend-second comes from app {{ msg }}</h1>
+    <h1>Config foo: {{ config.foo }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/runtime-core'
+
+export default defineComponent({
+  name: 'TestVue',
+  props: {
+    msg: String,
+    config: Object
+  }
+})
+</script>
